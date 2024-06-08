7dc37f6c43e5dc50fc7531c002279443d2af6d11
suresh <sureshvangara1619@gmail.com>
2024-06-08 12:38:49 +0530

    added mergesort

---
+++ mergesort.py
@@ -0,0 +1,21 @@
+def merge(left,right):
+    i,j=0,0
+    merged=[]
+    while i<len(left) and j<len(right):
+        if left[i]<=right[j]:
+            merged.append(left[i])
+            i+=1
+        else:
+            merged.append(right[j])
+            j+=1
+    merged+=left[i:]+right[j:]
+    return merged
+def mergesort(arr):
+    if len(arr)==1:
+        return arr
+    mid=len(arr)//2
+    left=mergesort(arr[:mid])
+    right=mergesort(arr[mid:])
+    return merge(left,right)
+arr=[3,4,2,1,6]
+print(mergesort(arr))
